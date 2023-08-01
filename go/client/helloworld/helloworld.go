e7acfe6aac0a8fce8487657b6584460d53145b47
Steven Fitzpatrick <steven@fitz.gg>
2023-08-01 14:57:20 -0700

    Update protobuf import

--- go/client/helloworld/helloworld.go
+++ go/client/helloworld/helloworld.go
@@ -3,22 +3,22 @@ package helloworld
 import (
 	"context"
 
-	hw "github.com/sf1tzp/learning-grpc/go/protobuf/helloworld"
+	pb "github.com/sf1tzp/learning-grpc/go/protobuf/helloworld"
 	"google.golang.org/grpc"
 )
 
 type HelloWorldClient struct {
-	client hw.GreeterClient
+	client pb.GreeterClient
 }
 
 func NewHelloWorldClient(connection *grpc.ClientConn) HelloWorldClient {
 	return HelloWorldClient{
-		client: hw.NewGreeterClient(connection),
+		client: pb.NewGreeterClient(connection),
 	}
 }
 
 func (c *HelloWorldClient) Hello(ctx context.Context, name string) (string, error) {
-	response, err := c.client.SayHello(ctx, &hw.HelloRequest{Name: name})
+	response, err := c.client.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		return "", err
 	}
@@ -26,7 +26,7 @@ func (c *HelloWorldClient) Hello(ctx context.Context, name string) (string, erro
 }
 
 func (c *HelloWorldClient) HelloAgain(ctx context.Context, name string) (string, error) {
-	response, err := c.client.SayHelloAgain(ctx, &hw.HelloRequest{Name: name})
+	response, err := c.client.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		return "", err
 	}
--- go/server/helloworld/helloworld.go
+++ go/server/helloworld/helloworld.go
@@ -5,28 +5,28 @@ import (
 	"fmt"
 	"log"
 
-	hw "github.com/sf1tzp/learning-grpc/go/protobuf/helloworld"
+	pb "github.com/sf1tzp/learning-grpc/go/protobuf/helloworld"
 	"google.golang.org/grpc"
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	hw.UnimplementedGreeterServer
+	pb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *hw.HelloRequest) (*hw.HelloReply, error) {
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	message := fmt.Sprintf("Hello to %s", in.GetName())
 	log.Printf("Info: %v", message)
-	return &hw.HelloReply{Message: message}, nil
+	return &pb.HelloReply{Message: message}, nil
 }
 
-func (s *server) SayHelloAgain(ctx context.Context, in *hw.HelloRequest) (*hw.HelloReply, error) {
+func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	message := fmt.Sprintf("Hello to %s again", in.GetName())
 	log.Printf("Info: %v", message)
-	return &hw.HelloReply{Message: message}, nil
+	return &pb.HelloReply{Message: message}, nil
 }
 
 func RegisterServer(s *grpc.Server) {
-	hw.RegisterGreeterServer(s, &server{})
+	pb.RegisterGreeterServer(s, &server{})
 }
