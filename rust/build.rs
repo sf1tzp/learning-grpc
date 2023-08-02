6854cfcc228ce68f8f4514606f6ab04d3a9ad2a5
Steven Fitzpatrick <steven@fitz.gg>
2023-08-02 01:21:48 -0700

    Troubleshooting rust build (proto imports)

--- rust/build.rs
+++ rust/build.rs
@@ -1,10 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // FIXME: Failing to compile, because of the import statement in routeguide.proto
+    // `"protoc failed: protobuf/googleapis/google/rpc/status.proto: File not found.\nrouteguide/routeguide.proto:5:1: Import \"protobuf/googleapis/google/rpc/status.proto\" was not found or had errors.`
     tonic_build::configure().out_dir("src/protobuf").compile(
-        &["../protobuf/helloworld/helloworld.proto"],
-        &["../protobuf"],
-    )?;
-    tonic_build::configure().out_dir("src/protobuf").compile(
-        &["../protobuf/routeguide/routeguide.proto"],
+        &[
+            "../protobuf/helloworld/helloworld.proto",
+            "../protobuf/routeguide/routeguide.proto",
+            // "../protobuf/googleapis/google/rpc/status.proto", 
+        ],
         &["../protobuf"],
     )?;
     Ok(())
