db7b46cf1e1b912532ce45fe42a99fecc73fcdd8
Steven Fitzpatrick <sfitzpatrick@microsoft.com>
2023-08-03 17:31:37 -0700

    Add client and server side streaming functions to node client

--- node/dist/client.js
+++ node/dist/client.js
@@ -23,16 +23,52 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const async = __importStar(require("async"));
 const grpc = __importStar(require("@grpc/grpc-js"));
+const lodash_1 = require("lodash");
 const helloworld_pb_1 = require("../protobuf/helloworld/helloworld_pb");
 const helloworld_grpc_pb_1 = require("../protobuf/helloworld/helloworld_grpc_pb");
+const routeguide_pb_1 = require("../protobuf/routeguide/routeguide_pb");
+const routeguide_grpc_pb_1 = require("../protobuf/routeguide/routeguide_grpc_pb");
+class Coordinate {
+    point = new routeguide_pb_1.Point();
+    constructor(latitude, longitude) {
+        this.point.setLatitude(latitude);
+        this.point.setLongitude(longitude);
+    }
+    toString() {
+        return "(" + this.point.getLatitude() + "," + this.point.getLongitude() + ")";
+    }
+}
+class SearchArea {
+    area = new routeguide_pb_1.Rectangle();
+    constructor(topLeft, bottomRight) {
+        this.area.setTopleft(topLeft.point);
+        this.area.setBottomright(bottomRight.point);
+    }
+}
 if (require.main === module) {
     main();
 }
 function main() {
-    var client = new helloworld_grpc_pb_1.GreeterClient('localhost:50051', grpc.credentials.createInsecure());
+    var helloClient = new helloworld_grpc_pb_1.GreeterClient('localhost:50051', grpc.credentials.createInsecure());
     var user = { name: 'Typescript' };
-    Hello(client, user);
+    Hello(helloClient, user);
+    var routeClient = new routeguide_grpc_pb_1.RouteGuideClient('localhost:50051', grpc.credentials.createInsecure());
+    var stl = new Coordinate(38.6270, -90.19940);
+    GetFeatureAt(routeClient, stl);
+    var topLeft = new Coordinate(50.030520, -126.800328);
+    var bottomRight = new Coordinate(20.104646, -75.219728);
+    var searchArea = new SearchArea(topLeft, bottomRight);
+    GetFeaturesIn(routeClient, searchArea);
+    var coords = [
+        new Coordinate(38.6270, -90.19940),
+        new Coordinate(39.7392, -104.9903),
+        new Coordinate(32.7157, -117.1611),
+        new Coordinate(37.7749, -122.4194),
+        new Coordinate(44.0570, -123.0869)
+    ];
+    getRouteDistance(routeClient, coords);
     return;
 }
 function Hello(client, user) {
@@ -41,10 +77,58 @@ function Hello(client, user) {
     client.sayHello(helloRequest, function (err, response) {
         if (err !== null) {
             console.log('Error: ', err.message);
+            client.close();
+        }
+        else {
+            console.log("In hello: ", response.getMessage());
+        }
+    });
+}
+function GetFeatureAt(client, coord) {
+    var foo = client.getFeature(coord.point, function (err, response) {
+        if (err !== null) {
+            console.log('Error: ', err.message);
+            client.close();
+        }
+        else {
+            console.log("Feature at: " + coord.toString() + " is " + response.getName());
+        }
+    });
+}
+function GetFeaturesIn(client, area) {
+    var call = client.listFeatures(area.area);
+    call.on("data", (chunk) => {
+        var name = chunk.getName();
+        var location = new Coordinate(chunk.getLocation().getLatitude(), chunk.getLocation().getLongitude());
+        console.log(name + " at " + location.toString());
+    });
+    call.on("error", (error) => {
+        console.log("Error: ", error.message);
+        client.close();
+    });
+    call.on("close", () => console.log("Stream ended"));
+}
+function getRouteDistance(client, coords) {
+    var call = client.recordRoute(function (error, response) {
+        if (error) {
+            console.log('Error: ', error.message);
             return;
         }
-        console.log("In hello: ", response.getMessage());
-        return;
+        else {
+            console.log("Route Distance: ", response.getDistance());
+        }
     });
+    function requestBuilder(coord) {
+        return function (callback) {
+            call.write(coord.point);
+            (0, lodash_1.delay)(callback, (0, lodash_1.random)(50, 100));
+        };
+    }
+    var requests = [];
+    for (var i = 0; i < coords.length; i++) {
+        var coord = coords[i];
+        requests[i] = requestBuilder(coord);
+    }
+    async.series(requests, () => call.end());
 }
 //# sourceMappingURL=client.js.map
--- node/src/client.ts
+++ node/src/client.ts
@@ -1,36 +1,134 @@
+import * as async from 'async'
+import * as grpc from '@grpc/grpc-js'
+import { delay, random } from 'lodash'
+import { HelloRequest, HelloReply } from '../protobuf/helloworld/helloworld_pb'
+import { GreeterClient } from '../protobuf/helloworld/helloworld_grpc_pb'
 
-import * as grpc from '@grpc/grpc-js';
-import { HelloRequest, HelloReply } from '../protobuf/helloworld/helloworld_pb';
-import { GreeterClient } from '../protobuf/helloworld/helloworld_grpc_pb';
-import { SurfaceCall } from '@grpc/grpc-js/build/src/call';
+import { Point, Rectangle, Feature, RouteNote} from '../protobuf/routeguide/routeguide_pb'
+import { RouteGuideClient } from '../protobuf/routeguide/routeguide_grpc_pb'
 
 type User = {
-    name: string;
+    name: string
+}
+
+class Coordinate {
+    point: Point = new Point()
+    constructor(latitude: number, longitude: number) {
+        this.point.setLatitude(latitude)
+        this.point.setLongitude(longitude)
+    }
+    toString() {
+        return "(" + this.point.getLatitude() + "," + this.point.getLongitude() + ")"
+    }
+}
+
+class SearchArea {
+    rectangle: Rectangle = new Rectangle()
+    constructor(topLeft: Coordinate, bottomRight: Coordinate) {
+        this.rectangle.setTopleft(topLeft.point)
+        this.rectangle.setBottomright(bottomRight.point)
+    }
 }
 
 if (require.main === module) {
-    main();
+    main()
 }
 
 function main() {
-    var client = new GreeterClient('localhost:50051', grpc.credentials.createInsecure());
-    var user: User = { name: 'Typescript' };
+    var helloClient = new GreeterClient('localhost:50051', grpc.credentials.createInsecure())
+    var user: User = { name: 'Typescript' }
 
-    Hello(client, user);
+    Hello(helloClient, user)
+
+    var routeClient = new RouteGuideClient('localhost:50051', grpc.credentials.createInsecure())
+    var stl: Coordinate = new Coordinate(38.6270,-90.19940)
+
+    GetFeatureAt(routeClient, stl)
+
+    var topLeft: Coordinate = new Coordinate(50.030520, -126.800328)
+    var bottomRight: Coordinate = new Coordinate(20.104646,  -75.219728)
+    var searchArea: SearchArea = new SearchArea(topLeft, bottomRight)
+
+    GetFeaturesIn(routeClient, searchArea)
+
+    var coords: Coordinate[] = [
+        new Coordinate(38.6270,-90.19940),
+        new Coordinate(39.7392,-104.9903),
+        new Coordinate(32.7157,-117.1611),
+        new Coordinate(37.7749,-122.4194),
+        new Coordinate(44.0570,-123.0869)
+    ]
+
+    getRouteDistance(routeClient, coords)
 
     return
 }
 
 function Hello(client: GreeterClient, user: User) {
-    var helloRequest = new HelloRequest();
-    helloRequest.setName(user.name);
+    var helloRequest = new HelloRequest()
+    helloRequest.setName(user.name)
 
     client.sayHello(helloRequest, function (err: grpc.ServiceError, response: HelloReply) {
         if (err !== null) {
-            console.log('Error: ', err.message);
-            return
+            console.log('Error: ', err.message)
+            client.close()
+        } else {
+            console.log("In hello: ", response.getMessage())
         }
-        console.log("In hello: ", response.getMessage());
-        return
-    });
+    })
+}
+
+// Route Guide APIs
+function GetFeatureAt(client: RouteGuideClient, coord: Coordinate) {
+    var foo = client.getFeature(coord.point, function (err: grpc.ServiceError, response: Feature) {
+        if (err !== null) {
+            console.log('Error: ', err.message)
+            client.close()
+        } else {
+            console.log("Feature at: " + coord.toString() + " is " + response.getName())
+        }
+    })
+}
+
+function GetFeaturesIn(client: RouteGuideClient, area: SearchArea) {
+    var call: grpc.ClientReadableStream<Feature> = client.listFeatures(area.rectangle)
+
+    call.on("data", (chunk: Feature) => {
+        var name = chunk.getName()
+        var location = new Coordinate(chunk.getLocation().getLatitude(), chunk.getLocation().getLongitude())
+        console.log(name + " at " + location.toString())
+    })
+
+    call.on("error", (error: grpc.ServiceError) => {
+        console.log("Error: ", error.message)
+        client.close()
+    })
+
+    call.on("close", () => console.log("Stream ended"))
+}
+
+function getRouteDistance(client: RouteGuideClient, coords: Coordinate[]) {
+    var call = client.recordRoute(function (error, response) {
+        if (error) {
+            console.log('Error: ', error.message)
+            return
+        } else {
+            console.log("Route Distance: ", response.getDistance())
+        }
+    })
+
+    function requestBuilder(coord: Coordinate) {
+        return function(callback) {
+            call.write(coord.point)
+            delay(callback, random(50, 100))
+        }
+    }
+
+    var requests = []
+    for (var i = 0; i < coords.length; i++) {
+        var coord = coords[i]
+        requests[i] = requestBuilder(coord)
+    }
+
+    async.series(requests, () => call.end())
 }
